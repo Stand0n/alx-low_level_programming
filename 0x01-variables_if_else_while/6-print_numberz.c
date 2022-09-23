910e02054ce78a6e8357316a688de5da5361bd17
Stand0n <stevemakavely@gmail.com>
2022-09-23 07:49:44 -0700

    Task 6. Numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 /**
- * main – Prints all single digit numbers of base 10 starting from 0,
- * only using putchar and without char variables.
+ * main – Entry point
  *
- * Return: Always 0.
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-int num;
-for (num = 0; num < 10; num++)
-putchar((num % 10) + ‘0’);
-putchar(‘\n’);
-return (0);
+	int d;
+
+	for (d = '0'; d <= '9'; d++)
+		putchar(d);
+	putchar('\n');
+	
+	return (0);
 }
