c6c0ef267612f1c096e08f7e78b0d0529c9ee999
Stand0n <stevemakavely@gmail.com>
2022-09-23 13:39:23 -0700

    Task 2. a function that prints 10 times the alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,4 +1,4 @@
-#include “main.h”
+#include "main.h"
 /**
  * print_alphabet_x10 - function to pribt abc
  * Return: x10 a-z
@@ -12,7 +12,7 @@ void print_alphabet_x10(void)
 {
 	for (alpha = 'a'; alpha <= 'z'; alpha++)
 {
-	_putchar(alpha);
+_putchar(alpha);
 }
 _putchar('\n');
 }
