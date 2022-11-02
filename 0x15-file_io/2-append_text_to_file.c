b3e8a8e6d4944e963ed3a9266b72434a96a43846
Stand0n <stevemakavely@gmail.com>
2022-11-02 00:43:19 -0700

    Task 2. Speak gently, she can hear

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,39 +1,28 @@
 #include "main.h"
 
 /**
- * append_text_to_file - appends text at the end of a file
- * @filename: filename.
- * @text_content: added content.
- *
- * Return: 1 if the file exists. -1 if the file does not exist
- * or if it fails.
- */
+* append_text_to_file - appends text at the end of a file
+* @filename: the filename
+* @text_content: the content to append
+*
+* Return: 1 (Success) otherwise, -1
+*/
 int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd;
-	int nletters;
-	int rwr;
+	unsigned int i;
+	int count;
 
-	if (!filename)
+	if (filename == NULL)
 		return (-1);
-
 	fd = open(filename, O_WRONLY | O_APPEND);
-
 	if (fd == -1)
 		return (-1);
-
-	if (text_content)
-	{
-		for (nletters = 0; text_content[nletters]; nletters++)
-			;
-		
-		rwr = write(fd, text_content, nletters);
-
-		if (rwr == -1)
-			return (-1);
-	}
-
-	close(fd);
-
+	i = 0;
+	while (text_content && *(text_content + i))
+		++i;
+	count = write(fd, text_content, i);
+	if (count  == -1)
+		return (-1);
 	return (1);
 }
