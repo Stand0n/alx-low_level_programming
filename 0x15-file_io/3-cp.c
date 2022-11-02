ad802a94e4316eb0bb54c33658f9c80da850122e
Stand0n <stevemakavely@gmail.com>
2022-11-02 00:54:17 -0700

    Task 3. cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,65 +1,71 @@
 #include "main.h"
-#include <stdio.h>
+
+#define PERM 0664
+#define BUFSIZE 1024
+
+void err(int fd, char mode, char *fn);
 
 /**
- * error_file - checks if files can be opened.
- * @file_from: file_from.
- * @file_to: file_to.
- * @argv: arguments vector.
- * Return: no return.
- */
-void error_file(int file_from, int file_to, char *argv[])
-{
-	if (file_from == -1)
-	{
-		dprintf(STDERR_FILENO, "Error, can't read from file %s\n", argv[1]);
-		exit(98);
-	}
-	if (file_to == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: can't write to %s\n", argv[2]);
-		exit(99);
-	}
-}
-
-/**
- * main - check the code for Holberton school students.
- * @argc: number of arguments.
- * @argv: arguments vector.
- * Return: Always 0.
- */
+* main - copies the content of a file to another file
+* @argc: no. of command line args
+* @argv: command line arguments
+*
+* Return: 0 (Success)
+*/
 int main(int argc, char *argv[])
 {
-	int file_from, file_to, err_close;
-	ssize_t nchars, nwr;
-	char buf[1024];
+	char buf[BUFSIZE];
+	int f1, f2, n;
 
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-
-	file_from = open(argv[1], O_RDONLY);
-	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
-	error_file(file_from, file_to, argv);
-
-	nchars = 1024;
-	while (nchars == 1024)
+	f1 = open(argv[1], O_RDONLY);
+	if (f1 == -1)
+		err(f1, 'r', argv[1]);
+	f2 = open(argv[2], O_WRONLY | O_TRUNC);
+	if (f2 == -1)
 	{
-		nchars = read(file_from, buf, 1024);
-		if (nchars == -1)
-			error_file(-1, 0, argv);
-		nwr = write(file_to, buf, nchars);
-		if (nwr == -1)
-			error_file(0, -1, argv);
+		f2 = open(argv[2], O_WRONLY | O_CREAT, PERM);
+		if (f2 == -1)
+			err(f2, 'w', argv[2]);
 	}
 
-	err_close = close(file_from);
-	if (err_close == -1)
+	while (((n = read(f1, buf, BUFSIZE)) > 0) && (n != -1))
+		if (write(f2, buf, n) != n)
+			err(f2, 'w', argv[2]);
+	if (n == -1)
+		err(f1, 'r', argv[1]);
+	if (close(f1) == -1)
+		err(f1, 'c', NULL);
+	if (close(f2) == -1)
+		err(f2, 'c', NULL);
+	return (0);
+}
+
+/**
+* err - handle errors
+* @fd: file descriptor
+* @mode: fd mode
+* @fn: filename
+*/
+void err(int fd, char mode, char *fn)
+{
+	if (mode == 'r')
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file _from);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", fn);
+		exit(98);
+	}
+	if (mode == 'w')
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", fn);
+		exit(99);
+	}
+	if (mode == 'c')
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
 		exit(100);
 	}
-	return (0);
 }
